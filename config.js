31eef7dbcf2b3ea71fb35a903ac3a7ea2072b278
a <creightons26@gmail.com>
2017-02-19 17:53:07 -0500

    Modified server: In production, caches static files; in development, does not.

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ module.exports = {
 	buildDirectory: 'public',
 	jsBuildName: 'build.js',
 	cssBuildName: 'build.css',
+	NODE_ENVIRONMENT: 'production',
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express'),
 	{
 		jsBuildName,
 		cssBuildName,
+		NODE_ENVIRONMENT
 	} = require('./config.js');
 
 // Set view/templates
@@ -14,13 +15,15 @@ app.set('views', 'views');
 // Set logging
 app.use(morgan('dev'));
 
-// Set static directory
-app.use(express.static("public"));
 
-process.env.NODE_ENVIRONMENT = 'production';
+const ONE_YEAR = 365 * 24 * 60 * 60 * 1000; // 1 Year in milliseconds
 
-let renderArgs;
-debugger;
+process.env.NODE_ENVIRONMENT = NODE_ENVIRONMENT;
+
+// Handle Cache Breaking
+let renderArgs,
+	staticOptions = {};
+
 if (process.env.NODE_ENVIRONMENT === 'production') {
 	if (!existsSync('manifest/manifest.json')) {
 		throw new Error('Need to build frontend assets before deploying production server.');
@@ -28,14 +31,25 @@ if (process.env.NODE_ENVIRONMENT === 'production') {
 	
 	const manifest = require('./manifest/manifest.json');
 	
+	// If in production, supply the index page with revisioned asset files
 	renderArgs = {
 		jsScript: manifest.jsBuildFile,
 		cssScript: manifest.cssBuildFile,
 	};
+	
+	// Cache production assets for one year. They will only be replaced when
+	// new revision reversions are made
+	staticOptions.maxage = ONE_YEAR;
 }
 else {
+	// If in development, use simple asset files
 	renderArgs = { jsScript: jsBuildName, cssScript: cssBuildName };
+	// Don't cache files in development.
+	staticOptions.maxage = 0;
 }
+
+// Set static directory
+app.use(express.static("public", staticOptions));
 
 // Render page
 app.get('/', (req, res, next) => res.status(200).render('index', renderArgs));
